e6d51a79ca67f5612c02c8c5cc7b826e42052987
DESKTOP-6LJVOB6\ksun0 <ksun0415canada@gmail.com>
2020-11-22 18:11:41 +0900

    少し、説明と新規登録までの誘導ページ（最初に出すページ、初めての人向け）

--- labo-layout/labo.js
+++ labo-layout/labo.js
@@ -1,13 +1,15 @@
 var mySwiper = new Swiper ('.swiper-container', {
-	effect: "flip",
-	loop: true,
-	pagination: '.swiper-pagination',
-	nextButton: '.swiper-button-next',
-	prevButton: '.swiper-button-prev',
-	breakpoints: {
+  loop: true,
+  slidesPerView: 2,
+  spaceBetween: 10,
+  centeredSlides : true,
+  pagination: '.swiper-pagination',
+  nextButton: '.swiper-button-next',
+  prevButton: '.swiper-button-prev',
+  breakpoints: {
     767: {
       slidesPerView: 1,
       spaceBetween: 0
-     }
     }
-  })
+  }
+})
