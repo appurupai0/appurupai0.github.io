f75013ac48e72b5b0e9c4a1c4d77e1b004abf807
DESKTOP-6LJVOB6\ksun0 <ksun0415canada@gmail.com>
2020-12-14 21:11:18 +0900

    ギャラリーページのレイアウト修正

--- amis-world/gallery-link-page/link-js/gallery-highway.js
+++ amis-world/gallery-link-page/link-js/gallery-highway.js
@@ -3,6 +3,18 @@ $(function(){
 		$('.js-shadow').addClass('on');
 	});
 
+	$('.nav-icon').on('click',function(){
+		$(this).addClass('none');
+		$('.nav-icon2').addClass('block');
+		$('.sideber').addClass('on');
+	});
+
+	$('.nav-icon2').on('click',function(){
+		$(this).removeClass('block');
+		$('.nav-icon').removeClass('none')
+		$('.sideber').removeClass('on');
+	});
+
 
 
 
