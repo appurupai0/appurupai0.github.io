25d64d5eacfe74050aba39f0c2210abe9bcb008c
DESKTOP-6LJVOB6\ksun0 <ksun0415canada@gmail.com>
2020-12-18 02:05:10 +0900

    galleryのマウスオーバー処理、もっと見るボタンの処理、ローディング処理を追加

--- amis-world/gallery-link-page/link-js/gallery-highway.js
+++ amis-world/gallery-link-page/link-js/gallery-highway.js
@@ -1,4 +1,29 @@
 $(function(){
+	// ローディング
+	let $window = $(window),
+        $load = $('#load_animation'),
+        $load_in = $load.find('img');
+
+    //ロゴとLoadingGIFをフェードイン
+	$load_in.delay(500).fadeIn('slow');
+	$('body, html').css('overflow-y', 'hidden');
+
+    //ウィンドウに対してloadイベントを設定
+    //全ての要素をロードが終わったら，HideLoadingScreen()を実行する
+    $window.on('load',function(){
+        HideLoadingScreen();
+    })
+    
+    //５秒後に強制的にHideLoadingScreen()を実行
+    setTimeout(function(){
+        HideLoadingScreen();
+    },5000);
+
+    //ロード画面を非表示にする関数
+    function HideLoadingScreen(){
+        $load.delay(1500).fadeOut("slow");
+    }
+
 	setTimeout(function(){
 		$('.js-shadow').addClass('on');
 	});
@@ -36,6 +61,14 @@ $(function(){
 	$('.mordal-close').on('click',function(){
 		$('.mordal').fadeOut();
 		$('body,html').css('overflow-y', 'visible');
+	});
+
+	$('.col-4').on('mouseover',function(){
+		$(this).addClass('shadow-on');
+	});
+
+	$('.col-4').on('mouseleave',function(){
+		$(this).removeClass('shadow-on');
 	});
 
 
--- amis-world/js/amis-world-script.js
+++ amis-world/js/amis-world-script.js
@@ -1,4 +1,7 @@
 $(function(){
+	
+
+
 	// ページの切り替えをタブメニューの応用で作る
 	$('.header-menu li a').on('click',function(){
 		console.log($(this).text());
@@ -100,6 +103,19 @@ $(function(){
 		
 		let isActive = $(this).hasClass('active');
 		toggleDrower(isActive);
+	});
+
+	//もっと見るボタンの動き
+	var show = 3; //最初に表示する件数
+	var num = 3;  //clickごとに表示したい件数
+	var contents = '.blog-news-block'; // 対象のlist
+	$(contents + ':nth-child(n + ' + (show + 1) + ')').addClass('is-hidden');
+	$('.all-blog span').on('click', function () {
+	$(contents + '.is-hidden').slice(0, num).removeClass('is-hidden');
+		if ($(contents + '.is-hidden').length == 0) {
+			$('.all-blog').fadeOut();
+			return false;
+		}
 	});
 
 
